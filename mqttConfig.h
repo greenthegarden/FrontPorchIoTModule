e729eb66a6217365a4fed44d754554556b687ecf
Philip Cutler <cutlervufamily@gmail.com>
2016-03-22 10:24:25 +1030

    update for consistency with other code

--- mqttConfig.h
+++ mqttConfig.h
@@ -1,13 +1,14 @@
-#ifndef FRONTPORCHIOTMODULE_MQTTCONFIG_H_
-#define FRONTPORCHIOTMODULE_MQTTCONFIG_H_
+#ifndef SENSORLIGHTIOTMODULE_MQTTCONFIG_H_
+#define SENSORLIGHTIOTMODULE_MQTTCONFIG_H_
 
 
 #include <PubSubClient.h>
 
+
 // MQTT parameters
 //byte mqtt_server_addr[]               = { 192, 168, 1, 30 };  // Airology
-byte mqtt_server_addr[]               = { 192, 168, 1, 55 };  // Pi eth0 interface
-char mqtt_client_id[]                 = "porchlight";
+byte mqttServerAddr[]                 = { 192, 168, 1, 55 };  // Pi eth0 interface
+char mqttClientId[]                   = "porchlight";
 const int MQTT_PORT                   = 1883;
 //#define MQTT_MAX_PACKET_SIZE            168
 //#define MQTT_KEEPALIVE                  300
@@ -22,17 +23,17 @@ char message[BUFFER_SIZE];
 
 // Status topics
 
-const char CONNECTED_STATUS[]   PROGMEM = "porchlight/status/connected";
-const char IP_ADDR_STATUS[]     PROGMEM = "porchlight/status/ip_addr";
-const char UPTIME_STATUS[]      PROGMEM = "porchlight/status/uptime";
-const char MEMORY_STATUS[]      PROGMEM = "porchlight/status/memory";
-const char TIME_STATUS[]        PROGMEM = "porchlight/status/time";
-const char LED_STATUS[]         PROGMEM = "porchlight/status/led";
-const char COLOUR_STATUS[]      PROGMEM = "porchlight/status/colour";
-const char PIR_STATUS[]         PROGMEM = "porchlight/status/pir";
-const char DHT22_STATUS[]       PROGMEM = "porchlight/status/dht22";
-const char TEMPERATURE_STATUS[] PROGMEM = "porchlight/status/temperature";
-const char HUMIDITY_STATUS[]    PROGMEM = "porchlight/status/humidity";
+const char CONNECTED_STATUS[]   PROGMEM = "sensorlight/status/connected";
+const char IP_ADDR_STATUS[]     PROGMEM = "sensorlight/status/ip_addr";
+const char UPTIME_STATUS[]      PROGMEM = "sensorlight/status/uptime";
+const char MEMORY_STATUS[]      PROGMEM = "sensorlight/status/memory";
+const char TIME_STATUS[]        PROGMEM = "sensorlight/status/time";
+const char LED_STATUS[]         PROGMEM = "sensorlight/status/led";
+const char COLOUR_STATUS[]      PROGMEM = "sensorlight/status/colour";
+const char PIR_STATUS[]         PROGMEM = "sensorlight/status/pir";
+const char DHT22_STATUS[]       PROGMEM = "sensorlight/status/dht22";
+const char TEMPERATURE_STATUS[] PROGMEM = "sensorlight/status/temperature";
+const char HUMIDITY_STATUS[]    PROGMEM = "sensorlight/status/humidity";
 
 PGM_P const STATUS_TOPICS[]     PROGMEM = { CONNECTED_STATUS,    // idx = 0
                                             IP_ADDR_STATUS,      // idx = 1
@@ -50,7 +51,7 @@ PGM_P const STATUS_TOPICS[]     PROGMEM = { CONNECTED_STATUS,    // idx = 0
 
 // Control topics
 
-const char LED_CONTROL[]        PROGMEM = "porchlight/control/led";
+const char LED_CONTROL[]        PROGMEM = "sensorlight/control/led";
 
 PGM_P const CONTROL_TOPICS[]    PROGMEM = { LED_CONTROL,         // idx = 0
                                           };
@@ -59,30 +60,30 @@ PGM_P const CONTROL_TOPICS[]    PROGMEM = { LED_CONTROL,         // idx = 0
 // callback function definition
 void callback(char* topic, uint8_t* payload, unsigned int length);
 
-PubSubClient   mqttClient(mqtt_server_addr, MQTT_PORT, callback, ethernetClient);
+PubSubClient   mqttClient(mqttServerAddr, MQTT_PORT, callback, ethernetClient);
 
 void publish_connected()
 {
-  prog_buffer[0] = '\0';
-  strcpy_P(prog_buffer, (char*)pgm_read_word(&(STATUS_TOPICS[0])));
-  mqttClient.publish(prog_buffer, "");
+  progBuffer[0] = '\0';
+  strcpy_P(progBuffer, (char*)pgm_read_word(&(STATUS_TOPICS[0])));
+  mqttClient.publish(progBuffer, "");
 }
 
 void publish_ip_address()
 {
-  prog_buffer[0] = '\0';
-  strcpy_P(prog_buffer, (char*)pgm_read_word(&(STATUS_TOPICS[1])));
-  mqttClient.publish(prog_buffer, "192.168.1.90");
+  progBuffer[0] = '\0';
+  strcpy_P(progBuffer, (char*)pgm_read_word(&(STATUS_TOPICS[1])));
+  mqttClient.publish(progBuffer, "192.168.1.90");
 }
 
 void publish_uptime()
 {
-  prog_buffer[0] = '\0';
-  strcpy_P(prog_buffer, (char*)pgm_read_word(&(STATUS_TOPICS[2])));
-  char_buffer[0] = '\0';
-  ltoa(millis(), char_buffer, 10);
-  mqttClient.publish(prog_buffer, char_buffer);
+  progBuffer[0] = '\0';
+  strcpy_P(progBuffer, (char*)pgm_read_word(&(STATUS_TOPICS[2])));
+  charBuffer[0] = '\0';
+  ltoa(millis(), charBuffer, 10);
+  mqttClient.publish(progBuffer, charBuffer);
 }
 
 
-#endif   /* FRONTPORCHIOTMODULE_MQTTCONFIG_H_ */
+#endif   /* SENSORLIGHTIOTMODULE_MQTTCONFIG_H_ */
