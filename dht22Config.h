fabb5b28fffdb3b7a7a4a7e5a5f2cc7d72ddcd04
Philip Cutler <cutlervufamily@gmail.com>
2016-03-22 11:02:00 +1030

    update for consistency with other code

--- dht22Config.h
+++ dht22Config.h
@@ -7,30 +7,30 @@
 
 dht DHT;
 
-const int DHT22_PIN               = 10;
+const int DHT22_PIN                     = 10;
 
-unsigned long dht22PreviousMillis = 0UL;
+unsigned long dht22PreviousMillis       = 0UL;
 
-const long dht22Interval          = 5UL * 60UL * 1000UL;           // interval at which to take measurement (milliseconds)
+const unsigned long DHT22_READ_INTERVAL = 5UL * 60UL * 1000UL;           // interval at which to take measurement (milliseconds)
 
 
 // DHT22 status messages
-const char DHT22_STATUS_OK[]                PROGMEM = "OK";
-const char DHT22_CHECKSUM_ERROR[]           PROGMEM = "Checksum error";
-const char DHT22_TIMEOUT_ERROR[]            PROGMEM = "Time out error";
-const char DHT22_CONNECT_ERROR[]            PROGMEM = "Connect error";
-const char DHT22_ACK_LOW_ERROR[]            PROGMEM = "Ack Low error";
-const char DHT22_ACK_HIGH_ERROR[]           PROGMEM = "Ack High error";
-const char DHT22_UNKNOWN_ERROR[]            PROGMEM = "Unknown error";
+const char DHT22_STATUS_OK[]        PROGMEM = "OK";
+const char DHT22_CHECKSUM_ERROR[]   PROGMEM = "Checksum error";
+const char DHT22_TIMEOUT_ERROR[]    PROGMEM = "Time out error";
+const char DHT22_CONNECT_ERROR[]    PROGMEM = "Connect error";
+const char DHT22_ACK_LOW_ERROR[]    PROGMEM = "Ack Low error";
+const char DHT22_ACK_HIGH_ERROR[]   PROGMEM = "Ack High error";
+const char DHT22_UNKNOWN_ERROR[]    PROGMEM = "Unknown error";
 
-PGM_P const DHT22_STATUS_MESSAGES[]   PROGMEM = { DHT22_STATUS_OK,        // idx = 0
-                                                  DHT22_CHECKSUM_ERROR,   // idx = 1
-                                                  DHT22_TIMEOUT_ERROR,    // idx = 2
-                                                  DHT22_CONNECT_ERROR,    // idx = 3
-                                                  DHT22_ACK_LOW_ERROR,    // idx = 4
-                                                  DHT22_ACK_HIGH_ERROR,   // idx = 5
-                                                  DHT22_UNKNOWN_ERROR,    // idx = 6
-                                                };
+PGM_P const DHT22_STATUS_MESSAGES[] PROGMEM = { DHT22_STATUS_OK,        // idx = 0
+                                                DHT22_CHECKSUM_ERROR,   // idx = 1
+                                                DHT22_TIMEOUT_ERROR,    // idx = 2
+                                                DHT22_CONNECT_ERROR,    // idx = 3
+                                                DHT22_ACK_LOW_ERROR,    // idx = 4
+                                                DHT22_ACK_HIGH_ERROR,   // idx = 5
+                                                DHT22_UNKNOWN_ERROR,    // idx = 6
+                                              };
 
 void publish_temperature_measurement()
 {
